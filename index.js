e6d0c7964993bda4df03226b5bb35ec3610fa193
sej-dev <wndmstla1@gmail.com>
2021-11-11 23:11:13 +0900

    Refactor: 비율 구하는 로직 개선

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import Guage from "./src/Guage.js";
 import RandomCheckbox from "./src/RandomCheckbox.js";
 
+/**
+ * 게이지 차트 컴포넌트와 체크박스 컴포넌트 생성한 후,
+ * 체크박스 change 이벤트 발생 시 게이지 차트 시뮬레이션 상태 변경
+ */
 window.addEventListener('DOMContentLoaded', () => {
     const guageChart = new Guage({
         $parent: document.querySelector("#app"),
